27118f4d4b4050371e3a71a56536c3c32a69016b
Seano <sean01234@yahoo.com>
2023-05-06 02:33:59 -0400

    add comments

--- scripts/app.js
+++ scripts/app.js
@@ -6,21 +6,59 @@ window.addEventListener("DOMContentLoaded",() => {
     let dropdown03=document.getElementById("catSelect3");
     let dropdown04=document.getElementById("catSelect4");
     let dropdown05=document.getElementById("catSelect5");
-    //dropdown01.addEventListener("change",(e) => {
-    //    //console.log(e.target.value);
-    //});
+    // if user reverts to empty select value, alert and clear array
+    dropdown01.addEventListener("change",(e) => {
+        if(dropdown01.value==''||dropdown02.value==''||dropdown03.value==''||dropdown04.value==''||dropdown05.value=='') {
+            missingAlertMsg.remove("hidden");
+            all_select_values=[]
+        } else {
+            missingAlertMsg.add("hidden");
+        }
+    });
+    dropdown02.addEventListener("change",(e) => {
+        if(dropdown01.value==''||dropdown02.value==''||dropdown03.value==''||dropdown04.value==''||dropdown05.value=='') {
+            missingAlertMsg.remove("hidden");
+            all_select_values=[]
+        } else {
+            missingAlertMsg.add("hidden");
+        }
+    });
+    dropdown03.addEventListener("change",(e) => {
+        if(dropdown01.value==''||dropdown02.value==''||dropdown03.value==''||dropdown04.value==''||dropdown05.value=='') {
+            missingAlertMsg.remove("hidden");
+            all_select_values=[]
+        } else {
+            missingAlertMsg.add("hidden");
+        }
+    });
+    dropdown04.addEventListener("change",(e) => {
+        if(dropdown01.value==''||dropdown02.value==''||dropdown03.value==''||dropdown04.value==''||dropdown05.value=='') {
+            missingAlertMsg.remove("hidden");
+            all_select_values=[]
+        } else {
+            missingAlertMsg.add("hidden");
+        }
+    });
+    dropdown05.addEventListener("change",(e) => {
+        if(dropdown01.value==''||dropdown02.value==''||dropdown03.value==''||dropdown04.value==''||dropdown05.value=='') {
+            missingAlertMsg.remove("hidden");
+            all_select_values=[]
+        } else {
+            missingAlertMsg.add("hidden");
+        }
+    });
 
     const startFlashCards=document.getElementById("startGame");
     const missingAlert=document.getElementById("needAllAlerts");
     const missingAlertMsg=missingAlert.classList;
 
-    startFlashCards.addEventListener("click",() => {
+    startFlashCards.addEventListener("click",(e) => {
+        e.preventDefault()
         if(dropdown01.value==''||dropdown02.value==''||dropdown03.value==''||dropdown04.value==''||dropdown05.value=='') {
-            //console.log('please add values for all 5 topics');
             missingAlertMsg.remove("hidden");
         } else {
             missingAlertMsg.add("hidden");
-            all_select_values=[]
+            all_select_values=[]; // reset array
             all_select_values.push(dropdown01.value,dropdown02.value,dropdown03.value,dropdown04.value,dropdown05.value)
             console.log(all_select_values);
         }
